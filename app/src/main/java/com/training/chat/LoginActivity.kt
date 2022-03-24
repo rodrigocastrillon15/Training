8f37ec252402cf763ac1b9030838a2da34301d57
rodrigocastrillon15 <rodrigocastrillon15@gmail.com>
2022-03-24 09:43:31 -0500

    Adding HomeActivity

--- app/src/main/java/com/training/chat/LoginActivity.kt
+++ app/src/main/java/com/training/chat/LoginActivity.kt
@@ -1,11 +1,32 @@
 package com.training.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    // ACÁ SE EMPIEZA A PROGRAMAR
+    // Declarar variable
+    private lateinit var buttonSign: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonSign = findViewById(R.id.button_sign)
+
+        buttonSign.setOnClickListener {
+            startHome()
+        }
+
     }
+
+    private fun startHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
 }
